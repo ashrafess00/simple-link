95d1f0cb95381526075d078a2a6d4aa20d044dcf
Achraf Essaoudi <aessaoud@c2r6p9.1337.ma>
2024-03-23 00:44:15 +0000

    Readme changed

--- app/register/verification/[userId]/page.js
+++ app/register/verification/[userId]/page.js
@@ -22,7 +22,7 @@ export default function Page({params}) {
             <div className="text-center text-2xl h-full  grid place-items-center">
                 <div>
                     <h1 className="text-2xl font-bold mb-5">Verification was sent to your email</h1>
-                    <p>you didn't receive the Verification link? <button onClick={resend} className="cusor-pointer text-violet-1">send again</button> </p>
+                    <p>you didn&apos;t receive the Verification link? <button onClick={resend} className="cusor-pointer text-violet-1">send again</button> </p>
                     <p className={`invisible ${sent && '!visible'} mt-5 text-lg font-thin text-white bg-dark bg-opacity-60 w-fit mx-auto p-2 px-6 rounded-full`}>Email verification was resent</p>
                     <Link href="/login" className="btn-secondary">Login</Link>
                 </div>
